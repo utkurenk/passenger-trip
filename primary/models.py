9694b9e8040fcbc43fee7cc70bacae712a22f549
utkurenk <egeutkurenk@gmail.com>
2022-09-16 01:55:53 +0300

    model changes and swagger

--- primary/models.py
+++ primary/models.py
@@ -1,4 +1,6 @@
+# from django.contrib.gis.db import models
 from django.db import models
+import geopy.distance
 
 
 class Passenger(models.Model):
@@ -6,13 +8,24 @@ class Passenger(models.Model):
     Email = models.EmailField(max_length=30)
     Country = models.CharField(max_length=60)
     City = models.CharField(max_length=60)
-    TotalDistanceCovered = models.IntegerField(default=0)
-    HomeLatitude = models.DecimalField(max_digits=12, decimal_places=10)
-    HomeLongitude = models.DecimalField(max_digits=12, decimal_places=10)
+    HomeLatitude = models.DecimalField(max_digits=9, decimal_places=6)
+    HomeLongitude = models.DecimalField(max_digits=9, decimal_places=6)
 
 
 class PassengerTrip(models.Model):
     CreatedAt = models.DateTimeField(auto_now_add=True)
-    DestinationLatitude = models.DecimalField(max_digits=12, decimal_places=10)
-    DestinationLongitude = models.DecimalField(max_digits=12, decimal_places=10)
     PassengerID = models.ForeignKey(Passenger, on_delete=models.CASCADE, null=True)
+    DestinationLatitude = models.DecimalField(max_digits=9, decimal_places=6)
+    DestinationLongitude = models.DecimalField(max_digits=9, decimal_places=6)
+    Distance = models.IntegerField(blank=True, default=0)
+
+    def calculate_distance(self):
+        coords_1 = (self.PassengerID.HomeLatitude, self.PassengerID.HomeLongitude)
+        coords_2 = (self.DestinationLatitude, self.DestinationLongitude)
+        return round(geopy.distance.geodesic(coords_1, coords_2).km)
+
+    def save(self,*args, **kwargs):
+        self.Distance = self.calculate_distance()
+        return super(PassengerTrip, self).save(*args, **kwargs)
+
+
